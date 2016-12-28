7fcad83ae0c2ac10d12a15d6e91d0cd4e67b2d44
ShravyaVanchanagiri <shravya.vanchanagiri@india.semanticbits.com>
2016-12-28 20:01:32 +0530

    This is a simple dashboard page

--- BookMyTicket-html/postTicketJs.js
+++ BookMyTicket-html/postTicketJs.js
@@ -1,6 +1,46 @@
-/**
- * Created by vanchanagiri shravya on 12/28/2016.
- */
-function onclick1() {
-    alert("Hiii");
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'MovieData.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+function PushTicket() {
+
+    var ticket = {};
+    loadJSON(function (response) {
+// Parse JSON string into object
+        var table = document.getElementById("userDetails");
+        var json_array = JSON.parse(response);
+        if(!localStorage.hasOwnProperty("jsonObject"))
+        {
+            localStorage.setItem("jsonObject", JSON.stringify(response));
+        }
+        else
+        {
+            json_array = JSON.parse(localStorage.getItem("jsonObject"));
+        }
+        console.log(json_array);
+
+
+            ticket.movie_name = document.getElementById("movie_name").value;
+            ticket.No_of_Tickets = document.getElementById("No_of_Tickets").value;
+            ticket.Cost = document.getElementById("Cost").value;
+            ticket.state = document.getElementById("state").value;
+            ticket.District = document.getElementById("District").value;
+            ticket.place = document.getElementById("place").value;
+            ticket.image = document.getElementById("image").value;
+            ticket.date = document.getElementById("date").value;
+            json_array.push(ticket);
+
+            localStorage.setItem("jsonObject",JSON.stringify(json_array));
+            console.log(JSON.parse(localStorage.getItem("jsonObject")));
+
+    });
 }
--- BookMyTicket-html/script.js
+++ BookMyTicket-html/script.js
@@ -4,7 +4,23 @@
 (function($) {
     $(function(){ // ON DOM READY
         $.getJSON('MovieData.json', function(movies) {
-            $.each(movies, function (key, val) {
+            console.log("movies === "+movies[0].movie_name);
+
+            var jsonObject =movies;
+
+            if(!localStorage.hasOwnProperty("jsonObject"))
+            {
+                console.log("has not");
+                localStorage.setItem("jsonObject",JSON.stringify(jsonObject));
+            }
+            else
+            {
+                console.log("has ");
+
+                 jsonObject = JSON.parse(localStorage.getItem("jsonObject"));
+            }
+            console.log(jsonObject);
+            $.each(jsonObject, function (key, val) {
                 html = '<div class="image-list">';
                 html += '<img src ="' + val.image + '" class="image-styles" />';
                 html += '<p class="image-title">' + val.movie_name + '</p><br>  ';
